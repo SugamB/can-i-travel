fba5854822bb4dc8e038a5722062dbe229998ee6
Sugam Banskota <Sugambanskota123@gmail.com>
2022-04-30 17:23:10 -0500

    responsive workin

---
+++ src/Card.js
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import './App.css'
+
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+  >
+    •
+  </Box>
+);
+export default function BasicCard({label, subHeading, textBody, bgColor, textColor}) {
+
+  return (
+    <Card className="card-class" style={{backgroundColor:`${bgColor}`, color:`${textColor}`}} >
+      <CardContent>
+        <Typography variant="h5" component="div">
+        {label}
+        </Typography>
+        <Typography sx={{ mb: 1.1 }} style={{ backgroundColor:`${bgColor}`, color:`${textColor}`}}>
+          {subHeading}
+        </Typography>
+        <Typography style={{textAlign: "justify"}} variant="body1">
+          <span dangerouslySetInnerHTML={{__html: textBody }} />
+        </Typography>
+      </CardContent>
+      <CardActions >
+        <Button style={{backgroundColor:`${bgColor}`}} size="small"> <a rel="noreferrer" href="https://www.immigration.com/blogs/f-1-students-travel-during-opt-or-h-1b-process" target="_blank"> Learn More </a></Button>
+      </CardActions>
+    </Card>
+  );
+}
